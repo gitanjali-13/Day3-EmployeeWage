74b697e3e457046731f990690bdb74f718123525
gitanjali-13 <gitaanjalide@gmail.com>
2022-09-11 21:52:10 +0530

    Check employee attendence

--- src/com/bridgelabz/employee/EmployeeWage.java
+++ src/com/bridgelabz/employee/EmployeeWage.java
@@ -3,5 +3,12 @@ package com.bridgelabz.employee;
 public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome to EMPLOYEE WAGES");
+        int fullTime = 1;
+        double check = Math.floor(Math.random() * 10 ) % 2;
+        if ( check == fullTime ) {
+            System.out.println("Employee is present.");
+        }else {
+            System.out.println("Employee is not present.");
+        }
     }
 }
