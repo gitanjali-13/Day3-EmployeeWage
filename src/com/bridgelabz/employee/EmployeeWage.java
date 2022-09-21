4762379617373317ab88a4b0efc09fb63c9020cd
gitanjali-13 <gitaanjalide@gmail.com>
2022-09-21 08:35:28 +0530

    added:part time

--- src/com/bridgelabz/employee/EmployeeWage.java
+++ src/com/bridgelabz/employee/EmployeeWage.java
@@ -5,18 +5,27 @@ public class EmployeeWage {
         System.out.println("Welcome to EMPLOYEE WAGES");
         int salary;
         int fullTime = 1;
+        int partTime = 2;
         int perHrsWage = 20;
+        int hrs = 0;
+        int empWage = 0;
         double check = Math.floor(Math.random() * 10 ) % 2;
-        if ( check == fullTime ) {
-            System.out.println("Employee is present.");
-            int Hrs = 8;
-            salary = perHrsWage * Hrs;
-            System.out.println("Salary of employee" +salary );
-        }else {
-            System.out.println("Employee is not present.");
-            int Hrs = 0;
-            salary = perHrsWage * Hrs;
-            System.out.println("Salary of employee"+salary);
+        if (check == fullTime)
+        {
+            System.out.println("Employee is Full time ");
+            hrs = 8;
+            empWage = perHrsWage * hrs;
+            System.out.println("Employe wage salary is " + empWage);
+        } else if (check == partTime) {
+            System.out.println("Employe is part time");
+            hrs =4;
+            empWage =perHrsWage * hrs;
+            System.out.println(empWage);
+        } else {
+            System.out.println("Employee is absent ");
+            hrs = 0;
+            empWage = perHrsWage * hrs;
+            System.out.println(empWage);
         }
     }
 }
