bc751585acd9339edc2ce0290b8284cf917b8e34
gitanjali-13 <gitaanjalide@gmail.com>
2022-09-21 08:46:34 +0530

    added:for loop

--- src/com/bridgelabz/employee/EmployeeWage.java
+++ src/com/bridgelabz/employee/EmployeeWage.java
@@ -3,31 +3,39 @@ package com.bridgelabz.employee;
 public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome to EMPLOYEE WAGES");
-        int salary;
-        int fullTime = 1;
-        int partTime = 2;
+
+        final int fullTime = 1;
+        final int partTime = 2;
         int perHrsWage = 20;
+        int empWorkingDays = 20;
         int hrs = 0;
         int empWage = 0;
+        int monthSalary =0;
         double check = Math.floor(Math.random() * 10 ) % 2;
-        switch (check){
-            case fullTime:
-                System.out.println("Employee is Full time ");
-                hrs = 8;
-                empWage = perHrsWage * hrs;
-                System.out.println("Employe wage salary is " + empWage);
-                break;
-            case partTime:
-                System.out.println("Employe is part time");
-                hrs =4;
-                empWage =perHrsWage * hrs;
-                System.out.println(empWage);
-                break;
-            default:
-                System.out.println("Employee is absent ");
-                hrs = 0;
-                empWage = perHrsWage * hrs;
-                System.out.println(empWage);
+        for (int i = 1; i < empWorkingDays; i++) {
+            int empCheck = (int) Math.floor(Math.random() * 10) % 2;
+            switch (empCheck) {
+                case fullTime:
+                    System.out.println("Employee is Full time ");
+                    hrs = 8;
+                    empWage = perHrsWage * hrs;
+                    System.out.println("Employe wage salary is " + empWage);
+                    break;
+                case partTime:
+                    System.out.println("Employe is part time");
+                    hrs = 4;
+                    empWage = perHrsWage * hrs;
+                    System.out.println(empWage);
+                    break;
+                default:
+                    System.out.println("Employee is absent ");
+                    hrs = 0;
+                    empWage = perHrsWage * hrs;
+                    System.out.println(empWage);
+            }
+            monthSalary = empWage+monthSalary;
         }
+        System.out.println("total month salary = "+monthSalary);
+    }
     }
 }
